90803e122a2c7285c54944e5ca976e177dc2bd3c
Marian Rybčanskyj <rybcanskyjmarian@gmail.com>
2020-12-06 22:19:02 +0100

    footer and cart css page

---
+++ Rybcansky_Shop/Components/CartTitleComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rybcansky_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rybcansky_Shop.Components
+{
+    public class CartTitleComponent : ViewComponent
+    {
+        public MyContext context { get; set; } = new MyContext();
+
+        public IViewComponentResult Invoke ()
+        {
+            //List<Cart_Item> list = this.context.Cart_Item.ToList();
+            
+            this.ViewBag.CartQuantity = this.context.Cart_Item.Count();
+
+            return View();
+        }
+    }
+}
