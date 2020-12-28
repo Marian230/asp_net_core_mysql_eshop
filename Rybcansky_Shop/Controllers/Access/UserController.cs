214f0d02af3e9a794f52d55f9b35d56c3ef722d2
Marian Rybčanskyj <rybcanskyjmarian@gmail.com>
2020-12-28 21:29:13 +0100

    homepage newsletter, some code, something in blog menu bar

--- Rybcansky_Shop/Controllers/Access/UserController.cs
+++ Rybcansky_Shop/Controllers/Access/UserController.cs
@@ -15,15 +15,27 @@ namespace Rybcansky_Shop.Controllers
 
         protected Random random { get; set; } = new Random();
 
+        public static double ExpireTime { get; set; } = 3600;
+
+        public static string userId { get; set; } = "userId";
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if (this.HttpContext.Session.GetString("userId") == null)
+            var orders = this.context.Order.Where(x => x.finished == false);
+
+            foreach (var item in orders)
+            {
+                if (DateTime.Now - item.last_Acces_Time > TimeSpan.FromSeconds(ExpireTime))
+                    this.context.Remove(this.context.Order.Find(item.id));
+            }
+            this.context.SaveChanges();
+
+            if (this.HttpContext.Session.GetString(UserController.userId) == null)
             {
                 int cookieID = this.random.Next(0, int.MaxValue);
-                //this.HttpContext.Session.Remove("userId");
-                this.context.Order.Add(new Order() { cookie_Id = cookieID });
+                this.context.Order.Add(new Order() { cookie_Id = cookieID, last_Acces_Time = DateTime.Now });
                 this.context.SaveChanges();
-                this.HttpContext.Session.SetString("userId", cookieID.ToString());
+                this.HttpContext.Session.SetString(UserController.userId, cookieID.ToString());
             }
 
             base.OnActionExecuting(context);
