af9ff3adc8616034fbef6709201b8e774f9fa6f9
Marian Rybčanskyj <rybcanskyjmarian@gmail.com>
2020-12-31 16:09:36 +0100

    Contact web page

---
+++ Rybcansky_Shop/Controllers/Web/Pages/Contact/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rybcansky_Shop.Controllers.Web.Pages.Contact
+{
+    public class ContactController : UserController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //TODO 
+        [HttpPost]
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
